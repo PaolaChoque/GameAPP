9219051ab3c6d50faee21e0331773626ef9209db
PaolaChoque <paolachristiane16@gmail.com>
2016-03-04 17:10:43 -0300

    segunda
    Adicionei canvas-container .na primeira tela.
    Adicionei novas tabelas sorte.

---
+++ js/configuracoes5.js
@@ -0,0 +1,7 @@
+$.confirm({
+    animation: 'zoom',
+    closeAnimation: 'scale'
+});
+// Available animations:
+// right, left, bottom, top, rotate, none, opacity, scale, zoom,
+// scaleY, scaleX, rotateY, rotateYR (reverse), rotateX, rotateXR (reverse)
