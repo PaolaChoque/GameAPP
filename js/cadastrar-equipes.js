86047688beea1225732ad6a059435502da5fa51a
PaolaChoque <paolachristiane16@gmail.com>
2016-03-10 16:46:33 -0300

    2016-03-10
    Criação de cadastrar reconhecimentos-conquista
    Cadastrar ações e ações.

--- js/cadastrar-equipes.js
+++ js/cadastrar-equipes.js
@@ -1,32 +1,112 @@
-$(".selector").gantt({
-	source: "ajax/data.json",
-	scale: "weeks",
-	minScale: "weeks",
-	maxScale: "months",
-	onItemClick: function(data) {
-		alert("Item clicked - show some details");
-	},
-	onAddClick: function(dt, rowId) {
-		alert("Empty space clicked - add an item!");
-	},
-	onRender: function() {
-		console.log("chart rendered");
-	}
-});
+ $(function() {
 
-source: [{
-	name: "Example",
-	desc: "Lorem ipsum dolor sit amet.",
-	values: [ ... ]
-}]
+            "use strict";
 
+            $(".gantt").gantt({
+                source: [{
+                    name: "Sprint 0",
+                    desc: "Analysis",
+                    values: [{
+                        from: "/Date(1320192000000)/",
+                        to: "/Date(1322401600000)/",
+                        label: "Requirement Gathering",
+                        customClass: "ganttRed"
+                    }]
+                },{
+                    desc: "Scoping",
+                    values: [{
+                        from: "/Date(1322611200000)/",
+                        to: "/Date(1323302400000)/",
+                        label: "Scoping",
+                        customClass: "ganttRed"
+                    }]
+                },{
+                    name: "Sprint 1",
+                    desc: "Development",
+                    values: [{
+                        from: "/Date(1323802400000)/",
+                        to: "/Date(1325685200000)/",
+                        label: "Development",
+                        customClass: "ganttGreen"
+                    }]
+                },{
+                    name: " ",
+                    desc: "Showcasing",
+                    values: [{
+                        from: "/Date(1325685200000)/",
+                        to: "/Date(1325695200000)/",
+                        label: "Showcasing",
+                        customClass: "ganttBlue"
+                    }]
+                },{
+                    name: "Sprint 2",
+                    desc: "Development",
+                    values: [{
+                        from: "/Date(1326785200000)/",
+                        to: "/Date(1325785200000)/",
+                        label: "Development",
+                        customClass: "ganttGreen"
+                    }]
+                },{
+                    desc: "Showcasing",
+                    values: [{
+                        from: "/Date(1328785200000)/",
+                        to: "/Date(1328905200000)/",
+                        label: "Showcasing",
+                        customClass: "ganttBlue"
+                    }]
+                },{
+                    name: "Release Stage",
+                    desc: "Training",
+                    values: [{
+                        from: "/Date(1330011200000)/",
+                        to: "/Date(1336611200000)/",
+                        label: "Training",
+                        customClass: "ganttOrange"
+                    }]
+                },{
+                    desc: "Deployment",
+                    values: [{
+                        from: "/Date(1336611200000)/",
+                        to: "/Date(1338711200000)/",
+                        label: "Deployment",
+                        customClass: "ganttOrange"
+                    }]
+                },{
+                    desc: "Warranty Period",
+                    values: [{
+                        from: "/Date(1336611200000)/",
+                        to: "/Date(1349711200000)/",
+                        label: "Warranty Period",
+                        customClass: "ganttOrange"
+                    }]
+                }],
+                navigate: "scroll",
+                scale: "weeks",
+                maxScale: "months",
+                minScale: "hours",
+                itemsPerPage: 10,
+                useCookie: true,
+                onItemClick: function(data) {
+                    alert("Item clicked - show some details");
+                },
+                onAddClick: function(dt, rowId) {
+                    alert("Empty space clicked - add an item!");
+                },
+                onRender: function() {
+                    if (window.console && typeof console.log === "function") {
+                        console.log("chart rendered");
+                    }
+                }
+            });
 
+            $(".gantt").popover({
+                selector: ".bar",
+                title: "I'm a popover",
+                content: "And I'm the content of said popover.",
+                trigger: "hover"
+            });
 
-values: [{
-	to: "/Date(1328832000000)/",
-	from: "/Date(1333411200000)/",
-	desc: "Something",
-	label: "Example Value",
-	customClass: "ganttRed",
-	dataObj: foo.bar[i]
-}]
+            prettyPrint();
+
+        });
